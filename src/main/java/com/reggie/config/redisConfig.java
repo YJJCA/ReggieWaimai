ba8aabefe3d0937bb1b9a8888b17dc3a15a82367
YJJCA <jjycanda@gmail.com>
2023-05-02 18:07:30 -0400

    redis

--- src/main/java/com/reggie/config/redisConfig.java
+++ src/main/java/com/reggie/config/redisConfig.java
@@ -1,4 +1,17 @@
 package com.reggie.config;
 
-public class redisConfig {
+import org.springframework.cache.annotation.CachingConfigurationSelector;
+import org.springframework.context.annotation.Bean;
+
+import java.text.StringCharacterIterator;
+
+public class RedisConfig extends CachingConfigurationSelector {
+    @Bean
+    public RedisTemplate<Object,Object> redisTemplate(RedisConnectionFactory connectionFactory){
+        RedisTemplate<Object,Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setConnectionFactory(connectionFactory);
+        return redisTemplate;
+    }
+
 }
